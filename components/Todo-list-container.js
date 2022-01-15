ad3c5a90584034de4d879bf4a60c96bfa189d12d
Tae <alsrb8442@naver.com>
2022-01-16 01:33:51 +0900

    💄 Add todo add button

--- components/Todo-list-container.js
+++ components/Todo-list-container.js
@@ -1,3 +1,4 @@
+import { AiFillPlusCircle } from 'react-icons/ai'
 import TodoItem from './Todo-item'
 
 export function TodoListContainer ({TodoList}) {
@@ -6,12 +7,31 @@ export function TodoListContainer ({TodoList}) {
       <ul className='todo-list'>
           {TodoList.map((item) => <TodoItem key={item.id} item={item} />)}
       </ul>
+      <button>
+        <AiFillPlusCircle />
+      </button>
       <style jsx>{`
         .todo-list {
           margin: 0;
+
+          // flex: 1;
+          
           padding: 16px;
           background-color: #ffffff;
           overflow: scroll;
+        }
+        button {
+          margin: 1rem 0 ;
+          display: flex;
+          justify-content: center;
+          background-color: #ffffff;
+          border: none;
+          color: lightgreen;
+          font-size: 3.5rem;
+          transition: transform 100ms ease-in-out;
+        }
+        button:hover {
+          transform: scale(1.2);
         }
       `}</style>
     </>
